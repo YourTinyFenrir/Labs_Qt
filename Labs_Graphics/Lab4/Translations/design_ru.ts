71591761c901c93d99dcca6a6abd0edbfa99537e
YourTinyFenrir <60516843+YourTinyFenrir@users.noreply.github.com>
2022-11-22 20:24:53 +0300

    Add files via upload

---
+++ Labs_Graphics/Lab4/Translations/design_ru.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Designer</source>
+        <translation>Дизайнер</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <source>&amp;LineEditBuddy</source>
+        <translation>&amp;Текст</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="59"/>
+        <source>&amp;BoxBuddy</source>
+        <translation>&amp;Счетчик</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="71"/>
+        <source>Exit</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <source>&amp;SliderBuddy</source>
+        <translation>&amp;Бегунок</translation>
+    </message>
+</context>
+</TS>
